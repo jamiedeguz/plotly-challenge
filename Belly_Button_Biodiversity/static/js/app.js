48115a4115cb0e95b2c60408b9b1d5e990bf5f90
jamiedg <jamiejdeguzman@gmail.com>
2019-11-07 01:23:18 -0800

    adding files

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -3,28 +3,65 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  var metadataURL = "/metadata/" + sample;
     // Use d3 to select the panel with id of `#sample-metadata`
+    var sampleMetadata = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    sampleMetadata.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    d3.json(metadataURL).then(function(data) {
+      Object.defineProperties(data).forEach(([key, value]) => {
+        sampleMetadata.append("h6").text(`${key}: ${value}`);
+    })
+    
+
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var chartsURL = "/samples/" + sample;
 
-    // @TODO: Build a Bubble Chart using the sample data
+  d3.json(chartsURL).then(function(data) {
+
+  // @TODO: Build a Bubble Chart using the sample data
+  var trace1 = {
+    x: data.otu_ids,
+    y: data,sample_values,
+    mode: 'markers',
+    text: data.otu_labels,
+    marker: {
+      color: data.otu_ids,
+      size: data.sample_values,
+      colorscale: "Earth"
+    }
+  };
+  
+  Plotly.newPlot('bubble', trace1, layout);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+    var data = [{
+      values: data.sample_values.slice(0,10),
+      labels: data.otu_ids.slice(0,10),
+      hovertext: data.otu_labels.slice(0,10),
+      type: 'pie',
+    }];
+
+    var layout = {
+      showlegend = true,
+    };
+
+    Plotly.newPlot('pie', data, layout);
+  }
+)}
 
 function init() {
   // Grab a reference to the dropdown select element
